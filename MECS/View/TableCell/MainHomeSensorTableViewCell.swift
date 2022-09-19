af56b21078819bcb24d6e4fe0646c1baea151891
Ethan-mo <ethan.mo@goodmonit.com>
2022-09-19 18:38:00 +0900

    migration : #8 MECS 프로젝트 생성과, UI작업 8 1. Table View Cell 관련, Swift파일 생성 및 초기 세팅

---
+++ MECS/View/TableCell/MainHomeSensorTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainHomeSensorTableViewCell.swift
+//  MECS
+//
+//  Created by 모상현 on 2022/09/19.
+//
+
+import UIKit
+
+class MainHomeSensorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
