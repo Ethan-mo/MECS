f36a246393a4cda7c5d800bcc98dbf75d22c825e
Ethan-mo <ethan.mo@goodmonit.com>
2022-09-28 17:20:09 +0900

    migration : #15 MECS 프로젝트 생성과, UI작업 15 1. ViewConroller연동작업 진행

---
+++ MECS/Controller/DeviceViewController_2.swift
@@ -0,0 +1,32 @@
+//
+//  DeviceViewController_2.swift
+//  MECS
+//
+//  Created by 모상현 on 2022/09/28.
+//
+
+import UIKit
+
+class DeviceViewController_2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func nextPageBtn(_ sender: UIButton) {
+        performSegue(withIdentifier: "2to3", sender: nil)
+    }
+}
